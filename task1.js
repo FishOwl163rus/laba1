b0c0f7e2d1baee4ac4540ab45e2c79a365dd2dca
Maksim Ramanau <MRamanau@ibagroup.eu>
2022-05-02 10:37:56 +0300

    Implement task with arguments

--- task1.js
+++ task1.js
@@ -5,32 +5,41 @@ const prompt = require("prompt-sync")({ sigint: true });
 
 program
     .option('-i, --input <string>', 'Specify input file')
-    .option('-o, --output <string>', 'Specify output file');
+    .option('-o, --output <string>', 'Specify output file')
+    .option('-a, --arguments <string>', 'Specify arguments');
 
 program.parse();
 
 let opts = program.opts();
 
 let input = opts.input;
+let output = opts.output;
+let arguments = opts.arguments;
 
 if (input === undefined || input === '') {
-    input = prompt("Specify input file: ");
-}
-
-let output = opts.output;
-
-if (output === undefined || input === '') {
-    output = prompt("Specify output file: ");
-}
-
-fs.readFile(input, 'utf8', function (err, content) {
-    if (err) return console.log(err);
-
-    let result = content.replace(/one|t[wh]|.i|[fsz]/g," $&").trimStart();
-
-    fs.writeFile(output, result, function (err) {
+    if (arguments === undefined || arguments === '') {
+        arguments = prompt("Specify arguments. Just enter string... ")
+    }
+    processTask(arguments, output);
+} else {
+    fs.readFile(input, 'utf8', function (err, content) {
         if (err) return console.log(err);
-        console.log('#TASK 1. The result was written to file.');
+        processTask(content, output);
     });
-});
+}
+
+function processTask(content, output) {
+    try {
+        if (output === undefined || output === '') {
+            console.log(content.replace(/one|t[wh]|.i|[fsz]/g, " $&").trimStart())
+        } else {
+            fs.writeFile(output, content.replace(/one|t[wh]|.i|[fsz]/g, " $&").trimStart(), function (err) {
+                if (err) return console.log(err);
+                console.log('#TASK 1. The result was written to file.');
+            });
+        }
+    } catch (e) {
+        console.log('An error occurred while processing arguments: ' + e)
+    }
+}
 
--- task2.js
+++ task2.js
@@ -5,43 +5,54 @@ const prompt = require("prompt-sync")({ sigint: true });
 
 program
     .option('-i, --input <string>', 'Specify input file')
-    .option('-o, --output <string>', 'Specify output file');
+    .option('-o, --output <string>', 'Specify output file')
+    .option('-a, --arguments <string>', 'Specify arguments');
 
 program.parse();
 
 let opts = program.opts();
 
 let input = opts.input;
+let output = opts.output;
+let arguments = opts.arguments;
 
 if (input === undefined || input === '') {
-    input = prompt("Specify input file: ");
-}
-
-let output = opts.output;
-
-if (output === undefined || input === '') {
-    output = prompt("Specify output file: ");
-}
-
-fs.readFile(input, 'utf8', function (err, content) {
-    if (err) return console.log(err);
-
-    function sum(A, B) {
-        let m = A.length, n = A[0].length, C = [];
-        for (let i = 0; i < m; i++) {
-            C[i] = [];
-            for (let j = 0; j < n; j++) C[i][j] = A[i][j] + B[i][j];
-        }
-        return C;
+    if (arguments === undefined || arguments === '') {
+        arguments = prompt("Specify arguments. Use format ([[..], [..]]:[[..], [..]]) ")
     }
-
-    let contents = content.split('\n');
-
-    let a = JSON.parse(contents[0]);
-    let b = JSON.parse(contents[1]);
-
-    fs.writeFile(output, JSON.stringify(sum(a, b)), function (err) {
+    processTask(arguments, output);
+} else {
+    fs.readFile(input, 'utf8', function (err, content) {
         if (err) return console.log(err);
-        console.log('#TASK 2. The result was written to file.');
+        processTask(content, output);
     });
-});
+}
+
+function processTask(content, output) {
+    try {
+        let contents = content.split(':');
+
+        let a = JSON.parse(contents[0]);
+        let b = JSON.parse(contents[1]);
+
+        if (output === undefined || output === '') {
+            console.log(sum(a, b))
+        } else {
+            fs.writeFile(output, JSON.stringify(sum(a, b)), function (err) {
+                if (err) return console.log(err);
+                console.log('#TASK 2. The result was written to file.');
+            });
+        }
+    } catch (e) {
+        console.log('An error occurred while processing arguments: ' + e)
+    }
+}
+
+function sum(a, b) {
+    let m = a.length, n = a[0].length, c = [];
+    for (let i = 0; i < m; i++) {
+        c[i] = [];
+        for (let j = 0; j < n; j++) c[i][j] = a[i][j] + b[i][j];
+    }
+    return c;
+}
